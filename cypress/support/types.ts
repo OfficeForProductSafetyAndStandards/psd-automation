29057848611208f1e617dc83535626c86f7d3047
Andy Callaghan <github@andycallaghan.com>
2023-05-02 15:42:37 +0100

    types and fixture for business, contact, location

--- cypress/support/types.ts
+++ cypress/support/types.ts
@@ -9,3 +9,26 @@ export type Product = {
   brand: string
   model: string
 }
+
+export type Address = {
+  addressLineOne: string;
+  addressLineTwo: string;
+  city: string;
+  postcode: string;
+  country: string;
+}
+
+export type Contact = {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  jobTitle: string;
+}
+
+export type Business = {
+  legalName: string;
+  tradingName: string;
+  companyNumber: string;
+  address: Address;
+  contact: Contact;
+}

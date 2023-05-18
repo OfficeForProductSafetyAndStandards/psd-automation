5ae031b7971118bdd349e8e652a4a4b7afdca9e1
Andy Callaghan <github@andycallaghan.com>
2023-05-18 10:45:29 +0100

    working esbuild + cucumber setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,48 +1,28 @@
-import { defineConfig } from 'cypress';
-import * as webpack from '@cypress/webpack-preprocessor';
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 export default defineConfig({
-  reporter: 'junit',
-  reporterOptions: {
-    mochaFile: 'results/my-test-output.xml',
-    toConsole: true,
-  },
   e2e: {
-    setupNodeEvents(on, config) {
+    specPattern: "**/*.feature",
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ): Promise<Cypress.PluginConfigOptions> {
+      // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+      await addCucumberPreprocessorPlugin(on, config);
 
       on(
         "file:preprocessor",
-        webpack({
-          webpackOptions: {
-            resolve: {
-              extensions: [".ts", ".js"],
-            },
-            module: {
-              rules: [
-                {
-                  test: /\.ts$/,
-                  exclude: [/node_modules/],
-                  use: [
-                    {
-                      loader: "ts-loader",
-                    },
-                  ],
-                },
-                {
-                  test: /\.feature$/,
-                  use: [
-                    {
-                      loader: "@badeball/cypress-cucumber-preprocessor/webpack",
-                      options: config,
-                    },
-                  ],
-                },
-              ],
-            },
-          },
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
         })
       );
+
+      // Make sure to return the config object as it might have been modified by the plugin.
+      return config;
     },
-    specPattern: '**/*.{feature,features}',
-    baseUrl: "https://docs.cypress.io",
-}});
+    baseUrl: "http://localhost:3000",
+  },
+});
